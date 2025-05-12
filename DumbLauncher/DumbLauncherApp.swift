8198a9038d0d4a407d1d04b347e58244e52e71ef
cmbaltimore44 <cmbaltimore@icloud.com>
2025-05-12 18:31:33 -0400

    Initial Commit

---
+++ DumbLauncher/DumbLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DumbLauncherApp.swift
+//  DumbLauncher
+//
+//  Created by Cooper Baltimore on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DumbLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DumbLauncherTests/DumbLauncherTests.swift
@@ -0,0 +1,16 @@
+//
+//  DumbLauncherTests.swift
+//  DumbLauncherTests
+//
+//  Created by Cooper Baltimore on 5/12/25.
+//
+
+import Testing
+
+struct DumbLauncherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
